29ffce3474c408221bbbdbb0187d27afa6161ab5
DmitriyVusyk <kotmurkot4502@gmail.com>
2020-08-15 21:06:18 +0300

    investigation of nullpointer issue

--- src/test/java/tests/LoginTest.java
+++ src/test/java/tests/LoginTest.java
@@ -3,21 +3,25 @@ package tests;
 import io.qameta.allure.*;
 import org.testng.annotations.Test;
 import pages.LoginPage;
+import pages.ProductsPage;
 
-import static utility.constants.Credentials.STANDART_USER_USERNAME;
+import static utility.constants.Credentials.PASSWORD_FOR_ALL_USERS;
+import static utility.constants.Credentials.STANDARD_USER_USERNAME;
 import static utility.constants.ErrorMessages.PASSWORD_IS_REQUIRED;
 import static utility.constants.ErrorMessages.USERNAME_IS_REQUIRED;
 
 @Epic("Base Tests")
 @Feature("Login Tests")
+@Test
 public class LoginTest extends BaseTest {
+    private LoginPage loginPage = new LoginPage(driver);
+    private ProductsPage productsPage = new ProductsPage(driver);
 
     @Test(priority = 1, description = "Invalid Login Scenario with empty username field.")
     @Severity(SeverityLevel.NORMAL)
     @Description("Test Description: Login test with empty username.")
     @Story("Empty username login test")
     public void invalidLoginTest_EmptyUserName() {
-        LoginPage loginPage = new LoginPage(driver);
         loginPage.clickLoginButton()
                 .isErrorMessageDisplayed(true)
                 .verifyErrorMessage(USERNAME_IS_REQUIRED);
@@ -28,10 +32,20 @@ public class LoginTest extends BaseTest {
     @Description("Test Description: Login test with correct username and empty password.")
     @Story("Correct username and empty password login test")
     public void invalidLoginTest_CorrectUserNameEmptyPassword() {
-        LoginPage loginPage = new LoginPage(driver);
-        loginPage.enterUsername(STANDART_USER_USERNAME)
+        loginPage.enterUsername(STANDARD_USER_USERNAME)
                 .clickLoginButton()
                 .isErrorMessageDisplayed(true)
                 .verifyErrorMessage(PASSWORD_IS_REQUIRED);
+    }
+
+    @Test(priority = 1, description = "Valid Login Scenario with correct username and correct password.")
+    @Severity(SeverityLevel.NORMAL)
+    @Description("Test Description: Login test with correct username and correct password.")
+    @Story("Correct username and correct password login test")
+    public void validLoginTest(){
+        loginPage.enterUsername(STANDARD_USER_USERNAME)
+                .enterPassword(PASSWORD_FOR_ALL_USERS)
+                .clickLoginButton();
+        productsPage.isCartButtonDisplayed();
     }
 }
--- src/test/java/utility/constants/Credentials.java
+++ src/test/java/utility/constants/Credentials.java
@@ -1,8 +1,9 @@
 package utility.constants;
 
 public final class Credentials {
-    public static final String STANDART_USER_USERNAME = "standard_user";
+    public static final String STANDARD_USER_USERNAME = "standard_user";
     public static final String LOCKED_OUT_USER_USERNAME = "locked_out_user";
     public static final String PROBLEM_USER_USERNAME = "problem_user";
     public static final String PERFORMANCE_GLITCH_USER_USERNAME = "performance_glitch_user";
+    public static final String PASSWORD_FOR_ALL_USERS = "secret_sauce";
 }
